88bc6333ec6f87b6fa6e4b9c7bcf8d1d1dadcea8
Frederico Ferreira <frederico.dida1@gmail.com>
2024-11-08 17:21:31 -0300

    first commit base

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <div class="collapse bg-dark" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">About</h4>
+            <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">Contact</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white">Follow on Twitter</a></li>
+              <li><a href="#" class="text-white">Like on Facebook</a></li>
+              <li><a href="#" class="text-white">Email me</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="container">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <i class="bi bi-view-stacked"></i>
+          <strong>Open Sprint Retro</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+  <RouterView />
+  <footer class="text-center">
+    <p>© 2024 Open Sprint Retro. Licensed under MIT License. Feel free to use and contribute! Create by <a href="https://github.com/FredericoSFerreira" class=""><i class="bi bi-github"></i> Frederico Ferreira </a></p>
+  </footer>
+
+</template>
+
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+footer {
+  position: fixed;
+  height: 100px;
+  bottom: 0;
+  width: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    newBoard() {
+      console.log("New Board")
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+  <main>
+  
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light">Gerencie as retrospectivas de suas sprints</h1>
+          <p class="lead text-muted">Colabore com sua equipe remota e melhore o que é feito utilizando uma ferramenta simples e intuitiva.</p>
+          <p>
+            <a href="#" class="btn btn-primary my-2 btn-lg" @click="newBoard()">Criar novo Board</a>
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
