f6fd32eadc94b3adb3bd64ad7f4200e0d4c0f294
Frederico Ferreira <frederico.dida1@gmail.com>
2025-06-25 17:30:28 -0300

    fix: test

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,36 @@
-import app from './index.js';
+import express from "express";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import cors from "cors";
+import {requestInfo} from "./middleware/request-info.js";
+import boardsRouter from "./modules/boards/routes/boards-routes.js";
+import authRouter from "./modules/auth/routes/auth-routes.js";
+import accessLogsRouter from "./modules/accessLogs/routes/access-logs-routes.js";
+import usersRouter from "./modules/users/routes/users-routes.js";
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: [process.env.FRONT_HOST, "http://localhost:5173"],
+  })
+);
+
+app.use(requestInfo);
+
+app.get("/healthcheck", (req, res) => {
+  res.send("OK");
+});
+
+app.use(boardsRouter);
+app.use(authRouter);
+app.use(accessLogsRouter);
+app.use(usersRouter);
+
+
 export default app;
