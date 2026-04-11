08aa53ba7a2d2950403ef8056380a51ee4264e31
Frederico Ferreira <frederico.dida1@gmail.com>
2026-04-11 16:02:10 -0300

    fix: migrations

--- backend/migrate-mongo-config.js
+++ backend/migrate-mongo-config.js
@@ -14,7 +14,7 @@ const config = {
     },
 
     // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-    migrationsDir: "backend/migrations",
+    migrationsDir: "migrations",
 
     // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
     changelogCollectionName: "changelog",
